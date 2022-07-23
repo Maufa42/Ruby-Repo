36101c3e79ac36e027b1432229860e229d7e3bb4
Home-Aryan <aryan.palongraph@gmail.com>
2022-07-23 14:10:04 +0530

    📖 Added Small Problems

---
+++ LaunchSchool/Exercises/RB101_small_problems/01_Easy/01_Repeat.rb
@@ -0,0 +1,13 @@
+# Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+# Example:
+
+# Copy Code
+# repeat('Hello', 3)
+
+
+def repeat (str,time)
+    time.times{puts str}
+end
+
+repeat("Kill",5)
